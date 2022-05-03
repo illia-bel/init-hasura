0d919f5b777a92ef93955b8bc9ef8c66af6fece0
ilya-belik <illia.belichenko@gmail.com>
2022-05-03 20:25:50 +0300

    fix

--- init.bash
+++ init.bash
@@ -3,5 +3,5 @@ curl https://raw.githubusercontent.com/ilya-belik/init-hasura/main/docker-compos
 npx -y hasura-cli init ./docker/hasura --endpoint http://localhost:4040 --admin-secret 'secret' && \
 
 curl https://raw.githubusercontent.com/ilya-belik/init-hasura/main/prod-env/.prod.env -o ./docker/hasura/.prod.env && \
-curl https://raw.githubusercontent.com/ilya-belik/init-hasura/main/prod-env/.env.secret -o ./docker/hasura/.env.secret
+curl https://raw.githubusercontent.com/ilya-belik/init-hasura/main/prod-env/.env.secret -o ./docker/hasura/.env.secret && \
 rm ./init.bash
