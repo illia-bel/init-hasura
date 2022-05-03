26a06b0739a429c6cd486825b33859ead8a52abb
ilya-belik <illia.belichenko@gmail.com>
2022-05-03 21:07:41 +0300

    dock

--- README.md
+++ README.md
@@ -7,6 +7,19 @@
 2. Download and start init file
 
    ```bash
-    curl https://raw.githubusercontent.com/ilya-belik/init-hasura/main/init.bash -o init.bash && \
-    sudo bash ./init.bash
+      curl https://raw.githubusercontent.com/ilya-belik/init-hasura/main/init.bash -o init.bash && \
+      sudo bash ./init.bash
    ```
+
+3. Connect database
+
+   ```sh
+      npx hasura-cli console --no-browser --project docker/hasura
+   ```
+
+   - **Open** [http://localhost:9695/console/data/manage/connect](http://localhost:9695/console/data/manage/connect)
+
+     Database Display Name: `default`  
+     Database URL: `postgres://postgres:secret@postgres:5432/postgres`
+
+     ![](./dock-1.png)
--- init.bash
+++ init.bash
@@ -5,7 +5,5 @@ npx -y hasura-cli init ./docker/hasura --endpoint http://localhost:4040 --admin-
 curl https://raw.githubusercontent.com/ilya-belik/init-hasura/main/files/.prod.env -o ./docker/hasura/.prod.env && \
 curl https://raw.githubusercontent.com/ilya-belik/init-hasura/main/files/.env.secret -o ./docker/hasura/.env.secret && \
 curl https://raw.githubusercontent.com/ilya-belik/init-hasura/main/files/config.yaml -o ./docker/hasura/config.yaml && \
-curl https://raw.githubusercontent.com/ilya-belik/init-hasura/main/files/databases.yaml -o ./docker/hasura/metadata/databases/databases.yaml && \
-curl https://raw.githubusercontent.com/ilya-belik/init-hasura/main/files/tables.yaml -o ./docker/hasura/metadata/databases/default/tables/tables.yaml && \
 
-# docker compose up -d
+docker compose up -d
